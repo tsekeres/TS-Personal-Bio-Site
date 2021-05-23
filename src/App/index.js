0a7cfaee20bb945a795c9605dc682fe2b7ae21fa
tsekeres <twsekeres@gmail.com>
2021-05-23 14:22:20 -0500

    getting auth firgured out

--- src/App/index.js
+++ src/App/index.js
@@ -1,32 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import firebase from 'firebase';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from '../helpers/Routes';
 import NavBar from '../components/NavBar';
-
-const adminUIDs = [
-  process.env.REACT_APP_ADMIN_TAD
-];
 
 function App() {
   const [admin, setAdmin] = useState(null);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((authed) => {
-      if (authed && adminUIDs.includes(authed.uid)) {
+      if (authed && (authed.uid === process.env.REACT_APP_ADMIN_TAD)) {
         setAdmin(true);
-        } else if (admin || admin === null) {
-          setAdmin(false);
-        }
+      } else if (admin || admin === null) {
+        setAdmin(false);
+      }
     });
   }, []);
 
   return (
     <div className='App'>
-      <Router>
-        <NavBar admin={admin} />
-        <Routes admin={admin} />
-      </Router>
+      <NavBar admin={admin} />
+      <Routes admin={admin} />
     </div>
   );
 }
--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types';
+// import React from 'react';
+// import { Route, Switch, Redirect } from 'react-router-dom';
+// import PropTypes from 'prop-types';
 
-const AuthedRoute = ({ component: Component, admin, ...rest }) => {
-  const routeChecker = (taco) =>
-    user ? (
-      <Component {...taco} user={user} />
-    ) : (
-      <Redirect to={{ pathname: '/', state: { from: taco.location } }} />
-    );
-  return <Route {...rest} render={(props) => routeChecker(props)} />;
-};
+// const AuthedRoute = ({ component: Component, admin, ...rest }) => {
+//   const routeChecker = (taco) =>
+//     admin ? (
+//       <Component {...taco} user={user} />
+//     ) : (
+//       <Redirect to={{ pathname: '/', state: { from: taco.location } }} />
+//     );
+//   return <Route {...rest} render={(props) => routeChecker(props)} />;
+// };
 
-AuthedRoute.propTypes = {
-  component: PropTypes.func,
-  user: PropTypes.any,
-};
+// AuthedRoute.propTypes = {
+//   component: PropTypes.func,
+//   admin: PropTypes.exact,
+// };
 
-function Routes({ user }) {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" user={user} component={Home} />
-        <PrivateRoute
-          exact
-          path="/"
-          user={user}
-          component={() => (
-            <ViewName user={user} />
-          )}
-        />
-        <PrivateRoute
-          exact
-          path="/"
-          user={user}
-          component={() => <ViewNAme user={user} />}
-        />
-        <Route path="*" component={Home} />
-      </Switch>
-    </div>
-  );
-}
+// function Routes({ admin }) {
+//   return (
+//     <div>
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route
+//           exact
+//           path="/"
+//           user={user}
+//           component={() => (
+//             <ViewName user={user} />
+//           )}
+//         />
+//         <PrivateRoute
+//           exact
+//           path="/"
+//           user={user}
+//           component={() => <ViewNAme user={user} />}
+//         />
+//         <Route path="*" component={Home} />
+//       </Switch>
+//     </div>
+//   );
+// }
 
-Routes.propTypes = {
-  user: PropTypes.any,
-};
+// Routes.propTypes = {
+//   user: PropTypes.any,
+// };
 
-export default Routes;
+// export default Routes;
